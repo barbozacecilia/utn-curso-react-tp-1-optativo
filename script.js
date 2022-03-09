ca394b5162a340677c8db618edc82771f8c65a3d
barbozacecilia <barboza.ceciliam@gmail.com>
2022-03-08 23:15:23 -0300

    styles(main): WIP, form styles using sibling combinator

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-function submitInfo() {
+/*function submitInfo() {
     var name = document.formContact.name.value
     var last_name = document.formContact.last_name.value
     var identification_number = document.formContact.identification_number.value
     var email = document.formContact.email.value
     var phone_number = document.formContact.phone_number.value
 
-    if (last_name === "") {
-        document.getElementById("e_last_name").innerHTML = "No se olvide de completar este campo"
+    if(name==="") {
+        document.getElementById("e_name").innerHTML = "Complete este campo"
     }
-       if (email === "") {
-        document.getElementById("e_email").innerHTML = "No se olvide de completar este campo"
+    if(last_name === "") {
+        document.getElementById("e_last_name").innerHTML = "Complete este campo"
     }
-    if (name === "") {
-        document.getElementById("e_name").innerHTML = "No se olvide de completar este campo"
+    if(email === "") {
+        document.getElementById("e_email").innerHTML = "Complete este campo"
     }
     console.log(name, last_name,identification_number,email, phone_number)
-}
+}*/
 
 function GetTypeOfInsurance() {
-    var typeOfInsurance = document.getElementById("type_of_insurance_id").value
+    let typeOfInsurance = document.getElementById("type_of_insurance_id").value
     switch (typeOfInsurance) {
         case "basic":
-            document.getElementById("cost_of_insurance").innerHTML="el valor del seguro por el que quiere consultar cuesta cuesta $500"
-            break
+            document.getElementById("cost_of_insurance").innerHTML="El valor del seguro por el que quiere consultar cuesta cuesta $500"
+            break;
         case "medium":
-            document.getElementById("cost_of_insurance").innerHTML ="el valor del seguro por el que quiere consultar cuesta $1000"
-            break
+            document.getElementById("cost_of_insurance").innerHTML ="El valor del seguro por el que quiere consultar cuesta $1000"
+            break;
         case "premium":
-            document.getElementById("cost_of_insurance").innerHTML= "el valor del seguro por el que quiere consultar cuesta $1500"
-             break
-        default: document.getElementById("cost_of_insurance").innerHTML= "el valor del seguro por el que quiere consultar cuesta cuesta $500"
-            break
+            document.getElementById("cost_of_insurance").innerHTML= "El valor del seguro por el que quiere consultar cuesta $1500"
+             break;
+        default:
+            document.getElementById("cost_of_insurance").innerHTML= "El valor del seguro por el que quiere consultar cuesta cuesta $500"
+            break;
     }
+}
+GetTypeOfInsurance()
+
+function SendForm() {
+    alert("Su formulario ha sido enviado!");
 }
